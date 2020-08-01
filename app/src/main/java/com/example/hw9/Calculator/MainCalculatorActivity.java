fcd566bf2b36f0acecb1668f1f9f57499ce1d67f
Rakhshanipr <you@example.com>
2020-08-01 11:19:37 +0430

    set initilas button in MainClaculatorActivity insert multiline in layout calculator

--- app/src/main/java/com/example/hw9/Calculator/MainCalculatorActivity.java
+++ app/src/main/java/com/example/hw9/Calculator/MainCalculatorActivity.java
@@ -3,11 +3,31 @@ package com.example.hw9.Calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.hw9.R;
 
 public class MainCalculatorActivity extends AppCompatActivity {
+    //region initialization
+    Button mButton1;
+    Button mButton2;
+    Button mButton3;
+    Button mButton4;
+    Button mButton5;
+    Button mButton6;
+    Button mButton7;
+    Button mButton8;
+    Button mButton9;
+    Button mButto0;
+    Button mButtonDevide;
+    Button mButtonPlus;
+    Button mButtonMultiple;
+    Button mButtonMinus;
+    Button mButtonDelete;
 
+
+
+    //endregion
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
